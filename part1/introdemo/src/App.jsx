3686e23008d0c215fa213b25059490cee6e0ebc8
Jason Ericson <jasonlericson@gmail.com>
2025-02-28 16:01:17 -0600

    Part 1d - Do Not Define Components Within Components

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 
+const Display = (props) => <div>{props.value}</div>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
 const History = (props) => {
   if (props.allClicks.length === 0) {
     return <div>the app is used by pressing the buttons</div>;
   }
   return <div>button press history: {props.allClicks.join(' ')}</div>;
 };
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = () => {
   const [left, setLeft] = useState(0);
@@ -45,10 +47,10 @@ const App = () => {
 
   return (
     <div>
-      {left}
+      <Display value={left} />
       <Button onClick={() => handleClick('L')} text={'left'} />
       <Button onClick={() => handleClick('R')} text={'right'} />
-      {right}
+      <Display value={right} />
       <History allClicks={allClicks} />
     </div>
   );
