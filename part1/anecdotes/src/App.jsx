4be22a2df5853dd41ef1e36512e0eae1ec46d546
Jason Ericson <jasonlericson@gmail.com>
2025-02-28 21:53:27 -0600

    Part 1 exercises 1.12-1.14 applied

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+
+const Anecdote = ({ heading, anecdote, votes }) => {
+  let pluralS = '';
+  pluralS = votes !== 1 ? 's' : '';
+
+  return (
+    <>
+      <h2>{heading}</h2>
+      <div>{anecdote}</div>
+      <div>
+        has {votes} vote{pluralS}
+      </div>
+    </>
+  );
+};
+
+const Button = ({ onClick, label }) => (
+  <button onClick={onClick}>{label}</button>
+);
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+  });
+  const [mostVotesIndex, setMostVotesIndex] = useState(0);
+  const [votesCast, setVotesCast] = useState(false);
+
+  const handleVoteClick = () => {
+    const votesCopy = { ...votes };
+    votesCopy[selected] += 1;
+    setVotes(votesCopy);
+    setVotesCast(true);
+
+    let mostIndex = 0;
+    for (let i = 1; i < 8; i++) {
+      if (votesCopy[i] > votesCopy[mostIndex]) {
+        mostIndex = i;
+      }
+    }
+    setMostVotesIndex(mostIndex);
+  };
+
+  const handleNextAnecdoteClick = () => {
+    setSelected(Math.floor(Math.random() * 8));
+  };
+
+  let mostVotesAnecdote = '';
+  if (votesCast) {
+    mostVotesAnecdote = (
+      <Anecdote
+        heading={'Anecdote with most votes'}
+        anecdote={anecdotes[mostVotesIndex]}
+        votes={votes[mostVotesIndex]}
+      />
+    );
+  }
+
+  return (
+    <div>
+      <Anecdote
+        heading={'Anecdote of the day'}
+        anecdote={anecdotes[selected]}
+        votes={votes[selected]}
+      />
+      <Button onClick={handleVoteClick} label={'vote'} />
+      <Button onClick={handleNextAnecdoteClick} label={'next anecdote'} />
+      {mostVotesAnecdote}
+    </div>
+  );
+};
+
+export default App;
