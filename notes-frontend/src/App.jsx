ccf47e9ff437f5bca876044c7ea5654413af96d1
Jason Ericson <jasonlericson@gmail.com>
2025-03-04 20:47:48 -0600

    Part 2c - Effect-hooks

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Note from './components/Note';
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    const eventHandler = (response) => {
+      setNotes(response.data);
+    };
+
+    const promise = axios.get('http://localhost:3001/notes');
+    promise.then(eventHandler);
+  };
+
+  useEffect(hook, []);
 
   const addNote = (event) => {
     event.preventDefault();
