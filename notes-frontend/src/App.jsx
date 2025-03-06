02024e4b0b6e19d68d137cb30724dd7bebc1ed08
Jason Ericson <jasonlericson@gmail.com>
2025-03-06 16:06:27 -0600

    Part 2e - Inline styles

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -3,6 +3,23 @@ import Note from './components/Note';
 import Notification from './components/Notification';
 import noteService from './services/notes';
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16,
+  };
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Notes app, Department of Computer Science, University of Helsinki 2025
+      </em>
+    </div>
+  );
+};
+
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
@@ -78,6 +95,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
