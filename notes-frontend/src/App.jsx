1ce9196d80b823c6dae3854a9cfd3718263ff8db
Jason Ericson <jasonlericson@gmail.com>
2025-03-05 19:43:18 -0600

    Part 2d - Extracting Communication with the Backend into a Separate Module

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import Note from './components/Note';
+import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    axios.get('http://localhost:3001/notes').then((response) => {
-      setNotes(response.data);
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
-  };
+  }, []);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -20,20 +20,18 @@ const App = () => {
       important: Math.random() < 0.5,
     };
 
-    axios.post('http://localhost:3001/notes', noteObject).then((response) => {
-      console.log(response);
-      setNotes(notes.concat(response.data));
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
       setNewNote('');
     });
   };
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    axios.put(url, changedNote).then((response) => {
-      setNotes(notes.map((note) => (note.id === id ? response.data : note)));
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
     });
   };
 
@@ -44,8 +42,6 @@ const App = () => {
   const notesToShow = showAll
     ? notes
     : notes.filter((note) => note.important === true);
-
-  useEffect(hook, []);
 
   return (
     <div>
